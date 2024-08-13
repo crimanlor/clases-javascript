07bc808180f35f248f397bbc07fafc3bd215cc60
Lorena Criado <criado.manzaneque@gmail.com>
2024-08-13 20:50:50 +0200

    Exercise 2 and 3 are working

--- ex3.js
+++ ex3.js
@@ -16,22 +16,29 @@ class DNI {
 
     // Nos devuelve 'true' o 'false' indicando si el DNI está caducado o no
     estaCaducado() {
-
+        const hoy = new Date();
+        const fechaCaducidad = new Date(this.caducidad);
+        return hoy > fechaCaducidad;
     }
 
     // Debe devolver 'true' si el DNI está bien formado, o 'false' en caso contrario
     esDniFormatoValido() {
-
+        const dniRegex = /^[0-9]{8}[A-Za-z]$/;
+        return dniRegex.test(this.numero);
     }
 
     // Dado un número de DNI, nos calcula la letra. Buscar por Internet como calcular la letra de un DNI
     calculaLetraDni(dni) {
-
+        const words = "TRWAGMYFPDXBNJZSQVHLCKET";
+        const number = parseInt(this.numero);
+        const i = number % 23
+        return words[i];
 
     }
 }
 
-const midni = new DNI("Pedro Vallés", "1234567A", "1990-10-10");
+const midni = new DNI("Pedro Vallés", "1234567", "1990-10-10");
 console.log(midni.estaCaducado());
 console.log(midni.esDniFormatoValido());
+console.log(midni.calculaLetraDni(this.numero));
 
